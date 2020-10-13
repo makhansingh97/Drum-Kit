0115eadb1d45f45c13ffe7cee22bfce53d76647c
Makhan Singh <makhansingh2897@gmail.com>
2020-10-13 12:26:28 +0530

    audio play test

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("I got clicked");
+    //   alert("I got clicked");
+
+    var audio = new Audio("sounds/crash.mp3");
+    audio.play();
 
     // What to do when clicked
   });
