56206215372c2ef85317b1e82019ff70e46b3e35
Makhan Singh <makhansingh2897@gmail.com>
2020-10-13 16:54:18 +0530

    button press and keypress response add

--- index.js
+++ index.js
@@ -1,46 +1,59 @@
 // adding event listener
 // using anonymous function
 
+// Detecting button press
+
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    var buttonInnerHTML = this.innerHTML;
-
-    switch (buttonInnerHTML) {
-      case "w":
-        var audio = new Audio("sounds/crash.mp3");
-        audio.play();
-        break;
-      case "a":
-        var audio = new Audio("sounds/kick-bass.mp3");
-        audio.play();
-        break;
-      case "s":
-        var audio = new Audio("sounds/snare.mp3");
-        audio.play();
-        break;
-      case "d":
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
-        break;
-      case "j":
-        var audio = new Audio("sounds/tom-2.mp3");
-        audio.play();
-        break;
-      case "k":
-        var audio = new Audio("sounds/tom-3.mp3");
-        audio.play();
-        break;
-      case "l":
-        var audio = new Audio("sounds/tom-4.mp3");
-        audio.play();
-        break;
-
-      // it'a good practice use default
-      default:
-        console.log(buttonInnerHTML);
-        break;
-    }
+    // var buttonInnerHTML = this.innerHTML;
+    makeSound(this.innerHTML);
   });
+}
+
+// Detecting keyboard Press
+
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
+});
+
+// funcition to make sound based on key passed
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+
+    // it'a good practice use default
+    default:
+      console.log(buttonInnerHTML);
+      break;
+  }
 }
