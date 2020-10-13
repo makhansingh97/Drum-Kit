81e8149a80616e8ee6399cf213bbe326f67a034c
Makhan Singh <makhansingh2897@gmail.com>
2020-10-13 13:50:59 +0530

    background images and audio added, responds on click

--- index.js
+++ index.js
@@ -5,11 +5,42 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    //   alert("I got clicked");
+    var buttonInnerHTML = this.innerHTML;
 
-    var audio = new Audio("sounds/crash.mp3");
-    audio.play();
+    switch (buttonInnerHTML) {
+      case "w":
+        var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+        break;
+      case "a":
+        var audio = new Audio("sounds/kick-bass.mp3");
+        audio.play();
+        break;
+      case "s":
+        var audio = new Audio("sounds/snare.mp3");
+        audio.play();
+        break;
+      case "d":
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+        break;
+      case "j":
+        var audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+        break;
+      case "k":
+        var audio = new Audio("sounds/tom-3.mp3");
+        audio.play();
+        break;
+      case "l":
+        var audio = new Audio("sounds/tom-4.mp3");
+        audio.play();
+        break;
 
-    // What to do when clicked
+      // it'a good practice use default
+      default:
+        console.log(buttonInnerHTML);
+        break;
+    }
   });
 }
